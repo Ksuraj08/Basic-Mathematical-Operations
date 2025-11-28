32aa932830e583b98dac9d8eb2bcd048b6885200
Ksuraj08 <kartiksuraj786@gmail.com>
2025-11-28 15:25:17 +0530

    Added files: 1. tasks.py for calculation 2. tasks.py for Greeting message
    Signed-off-by: Ksuraj08 <kartiksuraj786@gmail.com>

--- task1.py
+++ task1.py
@@ -1,1 +1,12 @@
-print("Hello Git.")
+num1 = int(input("enter your number: "))
+num2 = int(input("Enter another number: "))
+
+addition = (num1 + num2)
+substraction = (num1 - num2)
+multiplication = (num1 * num2)
+division = (num1 / num2)
+
+print("Addition: ", addition)
+print("subtraction: ", substraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
